82ec4ab0915ab303456ade33e86a12cd3b89f956
wmin0 <wmin0@hotmail.com>
2013-06-02 15:30:46 +0800

    move mid to socket.js

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,5 +1,12 @@
 (function(){
   "use strict";
+  var rid = /rid=([^&]+)/.exec(window.location.search);
+  if (!rid) {
+    console.log("no rid");
+  } else {
+    $("body").attr("data-room-id", rid[1]);
+  }
+
   var userId = "" + Math.random();
 
   var _socket = io.connect(window.iHaveQUrl || '/', {
