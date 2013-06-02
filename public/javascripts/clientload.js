a5d2d0dba4b148020b42b1646ff3b1b443f14afc
MrOrz <johnsonliang7@gmail.com>
2013-06-02 14:21:36 +0800

    Expose socket and iHaveQUrl.

--- public/javascripts/clientload.js
+++ public/javascripts/clientload.js
@@ -2,12 +2,13 @@
   var scriptList = [
     '/javascripts/dependencies.js',
     '/socket.io/socket.io.js',
+    '/javascripts/socket.js',
     '/javascripts/questions.js',
     '/javascripts/client.js'];
   var styleList = [
     '/stylesheets/style.css'
   ];
-  var rootUrl = 'http://localhost:3000';
+  window.iHaveQUrl = 'http://localhost:3000';
   function loadScript() {
     if (0 == scriptList.length) {
       return;
@@ -15,7 +16,7 @@
     var s = document.createElement('script');
     var t = document.getElementsByTagName('script')[0];
     s.type = 'text/javascript';
-    s.src = rootUrl + scriptList[0];
+    s.src = window.iHaveQUrl + scriptList[0];
     s.onload = loadScript;
     t.parentNode.insertBefore(s, t);
     scriptList.shift();
@@ -27,7 +28,7 @@
     var s = document.createElement('link');
     var t = document.getElementsByTagName('link')[0];
     s.rel = "stylesheet";
-    s.href = rootUrl + styleList[0];
+    s.href = window.iHaveQUrl + styleList[0];
     s.onload = loadStyle;
     t.parentNode.insertBefore(s, t);
     styleList.shift();
