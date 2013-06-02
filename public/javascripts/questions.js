5675e7c1c636251d0ac0968258bd70ed70632ff8
japie <jaywiewu@gmail.com>
2013-06-02 12:43:42 +0800

    Modify question.js

--- public/javascripts/questions.js
+++ public/javascripts/questions.js
@@ -1,79 +1,81 @@
-$(function() {
+var Socket = undefined;
 
-  var Question = Backbone.Model.extend({
-    initialize: function() {
+var Question = Backbone.Model.extend({
+  initialize: function() {
+  },
+  defaults: {
+    // slide location
+    'location': {
+      'x': 0,
+      'y': 0,
+      'pageurl': '',
     },
-    defaults: {
-      // slide location
-      'location': {
-        'x': 0,
-        'y': 0,
-        'pageurl': '',
-      },
-      // repeat or question
-      'type': 0,
-      // for question
-      'str': '',
-      // done, or to be answer
-      'state': 0,
-      // +1
-      'count': 0,
-    },
-  });
+    // repeat or question
+    'type': 0,
+    // for question
+    'str': '',
+    // done, or to be answer
+    'state': 0,
+    // +1
+    'count': 0,
+  },
+});
 
-  var Questions = Backbone.Collection.extend({
-    initialize: function(options) {
-    },
-    model: Question,
-  });
+var Questions = Backbone.Collection.extend({
+  initialize: function(options) {
+  },
+  model: Question,
+});
 
-  var QuestionView = Backbone.View.extend({
-    initialize: function() {
-      this.collection.bind('add', this.addQuestion);
-      this.collection.bind('change:state', this.changeQuestionState);
-      this.collection.bind('change:count', this.changeQuestionCount);
-      this.$el.append("<ul class='question-list'></ul>");
-    },
-    events: {
-      'click .done-btn': 'doneQuestion',
-      'click .plus-btn': 'plusOneQuestion',
-    },
-    addQuestion: function(model) {
-      // TODO: socket.io.emit
-      console.log(model);
-      var obj = $("<li></li>");
-      obj.attr("mid", model.cid);
-      console.log(model.get('location'));
-      if (1 == model.get('type')) {
-        obj.html(model.get('str'));
-      } else {
-        obj.html("repeat please!!");
-      }
-      obj.append($("<button class='done-btn'>done</button><button class='plus-btn'>+1</button>"));
-      $('.question-list', this.$el).append(obj);
-    },
-    changeQuestionState: function(model) {
-      // TODO: socket.io.emit
-      console.log('changeQuestionState', model);
-    },
-    changeQuestionCount: function(model) {
-      // TODO: socket.io.emit
-      console.log('changeQuestionCount', model);
-    },
-    doneQuestion: function(event) {
-      var id = $(event.target).parent().attr('mid');
-      var model = this.collection.get(id);
-      var state = model.get('state');
-      model.set('state', 1);
-    },
-    plusOneQuestion: function(event) {
-      var id = $(event.target).parent().attr('mid');
-      var model = this.collection.get(id);
-      var count = model.get('count');
-      model.set('count', count + 1);
-    },
-  });
-
+var QuestionView = Backbone.View.extend({
+  initialize: function() {
+    this.collection.bind('add', this.addQuestion);
+    this.collection.bind('change:state', this.changeQuestionState);
+    this.collection.bind('change:count', this.changeQuestionCount);
+    // this.$el.append("<ul class='question-list'></ul>");
+  },
+  events: {
+    'click .content': 'doneQuestion',
+    'click .plus': 'plusOneQuestion',
+  },
+  addQuestion: function(model) {
+    // TODO: socket.io.emit
+    console.log(model);
+    var obj = $('<div class="one-question"><div class="content"></div><div class="plus"></div></div>');
+    obj.attr("mid", model.cid);
+    // console.log(model.get('location'));
+    if (1 == model.get('type')) {
+      $(".content", obj).html(model.get('str'));
+    } else {
+      $(".content", obj).html("repeat please!!");
+    }
+    // obj.append($("<button class='done-btn'>done</button><button class='plus-btn'>+1</button>"));
+    this.$el.append(obj);
+  },
+  changeQuestionState: function(model) {
+    // TODO: socket.io.emit
+    console.log('changeQuestionState', model);
+  },
+  changeQuestionCount: function(model) {
+    // TODO: socket.io.emit
+    console.log('changeQuestionCount', model);
+  },
+  doneQuestion: function(event) {
+    var id = $(event.target).parent().attr('mid');
+    $(event.target)
+    console.log(id);
+    var model = this.collection.get(id);
+    var state = model.get('state');
+    model.set('state', 1);
+  },
+  plusOneQuestion: function(event) {
+    var id = $(event.target).parent().attr('mid');
+    var model = this.collection.get(id);
+    var count = model.get('count');
+    model.set('count', count + 1);
+  },
+});
+/*
   var questionCollection = new Questions();
 
   var view = new QuestionView({
@@ -98,4 +100,5 @@ $(function() {
       'count': 1,
     }));
   });
-});
+
+*/
