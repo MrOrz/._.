0e527abbfde05b5cfdb4c736c9bca519ffcb5fc0
japie <jaywiewu@gmail.com>
2013-06-02 13:57:15 +0800

    Tool box

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,3 +1,16 @@
 $(function() {
   console.log(Questions);
+  var stringBuild = [
+  	'<div class="ihq-tool-box">',
+  		'<div class="ihg-tool">',
+  			'<i class="icon-search">',
+  			'</i>',
+  			'<div> class="ask"',
+  			'</div>',
+  		'</div>',
+  		'<div class="ihg-question-queue">',  			
+  		'</div>',
+  	'</div>'
+  ];
+  $('body').append(stringBuild.join(''));
 });
