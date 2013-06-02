aa93a1938264b40fb330692fd74e3ad5190b77c6
wmin0 <wmin0@hotmail.com>
2013-06-02 12:14:08 +0800

    add connect in test.js

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,6 +1,8 @@
 (function(){
   "use strict";
-  var socket = io.connect();
+  var socket = io.connect('/', {
+    query: 'roomId=' + $('body').attr('data-room-id')
+  });
   socket.on('connect', function (data) {
     console.log('Connected');
   });
