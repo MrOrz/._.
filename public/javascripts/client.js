13c5c7dbefc80337f631304786d83a1b2d2c41ab
wmin0 <wmin0@hotmail.com>
2013-06-02 14:23:28 +0800

    start client router

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,16 +1,36 @@
 $(function() {
   console.log(Questions);
+  var rid = /rid=([^&]+)/.exec(window.location.search)[1];
+  if (!rid) {
+    console.log("no rid");
+    return;
+  }
   var stringBuild = [
-    '<div class="ihq-tool-box">',   
+    '<div class="ihq-tool-box">',
       '<div class="ihg-tool">',
         '<i class="icon-search">',
         '</i>',
         '<div> class="ask"',
         '</div>',
       '</div>',
-      '<div class="ihq-question-queue">',       
+      '<div class="ihq-question-queue">',
       '</div>',
     '</div>'
   ];
   $('body').append(stringBuild.join(''));
+  var questionCollection = new Questions();
+  var view = new QuestionView({
+    el: $(".ihq-question-queue"),
+    collection: questionCollection
+  });
+
+  var Router = Backbone.Router.extend({
+    routes: {
+      ":foo": "func1",
+    },
+    func1: function(p1) {
+        console.log('func1: '+p1);
+    },
+  });
+
 });
