dc7edbd03ed447ae441221933f52ec47acee7db6
MrOrz <johnsonliang7@gmail.com>
2013-06-02 15:25:27 +0800

    Current-user aware socket.js

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,9 +1,28 @@
 (function(){
   "use strict";
-  window.socket = io.connect(window.iHaveQUrl || '/', {
+  var userId = "" + Math.random();
+
+  var _socket = io.connect(window.iHaveQUrl || '/', {
     query: 'roomId=' + $('body').attr('data-room-id')
   });
-  window.socket.on('connect', function (data) {
+  _socket.on('connect', function (data) {
     console.log('Connected');
   });
+
+  // Current-user aware socket.
+  window.socket = {
+    emit: function(evt, data){
+      // Append user id in data.
+      data.userId = userId;
+      _socket.emit(evt, data);
+    },
+    on: function(evt, callback){
+      // Check user id.
+      _socket.on(evt, function(data){
+        if(data.userId !== userId){
+          callback(data);
+        }
+      });
+    }
+  };
 }());
